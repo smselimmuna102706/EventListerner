7ff7673ccd4e7b4be9c425856996c7b2f5428af2
smselimmuna102706 <111196494+smselimmuna102706@users.noreply.github.com>
2022-09-14 20:37:26 +0600

    Add files via upload

---
+++ 43.js
@@ -0,0 +1,16 @@
+
+var len = document.querySelectorAll(".myButton").length;
+
+for( var i =0; i < len; i++){
+    document.querySelectorAll(".myButton")[i].addEventListener("click",function(){
+
+
+        var text = this.innerHTML;
+        document.querySelector("h1").innerHTML = text + "  is clicked";
+    
+    
+    });
+}
+
+
+
